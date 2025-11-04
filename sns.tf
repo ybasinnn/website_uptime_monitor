e399fb855f409049e49f63e3250d38cfd32011d2
ybasinnn <yordan.basin1337@gmail.com>
2025-11-04 04:51:56 +0200

    feat: sns config

---
+++ sns.tf
@@ -0,0 +1,11 @@
+resource "aws_sns_topic" "health_check_topic" {
+  name = "WebsiteHealthCheckTopic"
+}
+
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.health_check_topic.arn
+  protocol  = "email"
+  endpoint  = var.email_endpoint
+  # NOTE: You must manually confirm the email subscription after Terraform applies!
+}
